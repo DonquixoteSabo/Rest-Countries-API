1cd825b8ee8cf4e553fff377d3aad1a870115d05
DonquixoteSabo <mixboksu@gmail.com>
2021-02-08 20:02:26 +0100

    add fetchCountries and Card component(without my own styles)

--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -1,5 +1,31 @@
-const CountriesList = () => {
-    return (<p>lista krajów yoł ziom</p>);
+import { Container, Card, CardMedia, CardContent, Typography } from '@material-ui/core';
+
+const CountriesList = ({ searchValue, countries }) => {
+
+    return (
+        <Container>
+            {countries.map(country => (
+                <Card key={country.key} className='card'>
+                    <CardMedia className='card__media' src={country.flag} title='country flag' component='img' />
+                    <CardContent className='card__content'>
+                        <Typography className='card__typography card__typography-name' gutterBottom variant="h5" component="h2">
+                            {country.name}
+                        </Typography>
+                        <Typography className='card__typography' gutterBottom variant="h6" component="h3">
+                            <span className='card__typography-title'>Population:</span> {country.population}
+                        </Typography>
+                        <Typography className='card__typography' gutterBottom variant="h6" component="h3">
+                            <span className='card__typography-title'>Region:</span> {country.region}
+                        </Typography>
+                        <Typography className='card__typography' gutterBottom variant="h6" component="h3">
+                            <span className='card__typography-title'>Capital</span>: {country.capital}
+                        </Typography>
+                    </CardContent>
+                </Card>
+            ))}
+
+        </Container>
+    );
 }
 
 export default CountriesList;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,50 @@
-import { useState } from 'react';
-import { Container } from '@material-ui/core';
+import { useState, useEffect} from 'react';
+
+import axios from 'axios';
+import {nanoid} from 'nanoid';
 
 import CountriesList from './CountriesList';
 import SearchBar from './SearchBar';
 
 const Main = () => {
     const [searchValue, setSearchValue] = useState('');
+    const [countries, setCountries] = useState([{
+        name: '',
+        population: '',
+        region: '',
+        capital: '',
+        flag: '',
+        key: '',
+    }]);
+
     const handleSearchInputChange = event => setSearchValue(event.target.value);
+    
+    useEffect(() => {
+        const fetchCountries = async () => {
+            const response = await axios.get('https://restcountries.eu/rest/v2/all');
+            let countries = response.data;
+            //I know I can destructure country values here but In this example I think this syntax is more clear
+            countries = countries.map(country => ({
+                name: country.name,
+                population: country.population,
+                region: country.region,
+                capital: country.capital,
+                flag: country.flag,
+                key: nanoid(8),
+            }))
+            console.log(countries)
+            setCountries(countries);
+        }
+        fetchCountries();
+    }, [])
 
     return(
-        <Container>
+        <>
             <SearchBar 
             searchValue={searchValue} 
             handleSearchInputChange={handleSearchInputChange} />
-            <CountriesList />
-        </Container>
+            <CountriesList searchValue={searchValue} countries={countries} />
+        </>
     ) 
 }
  
