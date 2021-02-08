175700fe7aead003a18b3d44a18c04ec70c75f73
DonquixoteSabo <mixboksu@gmail.com>
2021-02-08 07:14:46 +0100

    create basic strucure to SearchBar Component.   I will add styles and filter func later

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,20 @@
+import { useState } from 'react';
+import { Container } from '@material-ui/core';
+
 import CountriesList from './CountriesList';
 import SearchBar from './SearchBar';
+
 const Main = () => {
+    const [searchValue, setSearchValue] = useState('');
+    const handleSearchInputChange = event => setSearchValue(event.target.value);
+
     return(
-        <>
-            <SearchBar />
+        <Container>
+            <SearchBar 
+            searchValue={searchValue} 
+            handleSearchInputChange={handleSearchInputChange} />
             <CountriesList />
-        </>
+        </Container>
     ) 
 }
  
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,28 @@
-const SearchBar = () => {
-    return (<h1>SearchBar</h1>);
+import { Input, InputAdornment, Select } from '@material-ui/core';
+import { Search } from '@material-ui/icons';
+
+const SearchBar = ({ searchValue, handleSearchInputChange }) => {
+
+    return (
+        <div className='wrapper'>
+            <Input
+                className='search__input'
+                value={searchValue}
+                onChange={handleSearchInputChange}
+                type='text'
+                startAdornment={
+                    <InputAdornment position="start">
+                        <Search />
+                    </InputAdornment>
+                }
+            />
+            <Select className='search__select'>
+                <option>siema1</option>
+                <option>siema2</option>
+                <option>siema3</option>
+            </Select>
+        </div>
+    );
 }
 
 export default SearchBar;
